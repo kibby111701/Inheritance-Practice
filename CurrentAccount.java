cecc1cf61601e8c8946aaa8b6f5d4f56dbf0d169
Christopher Michael Kraft <kraftcm@s.dcsdk12.org>
2020-01-29 08:35:50 -0700

    quick fix to current account

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -6,4 +6,16 @@ public class CurrentAccount extends Account{
         this.overdraftLimit = o;
     }
 
+    public String letter(){
+        return "Boi stop spending so much money";
+    }
+
+    public void withdraw(double sum){
+        if (sum > overdraftLimit){
+            letter();
+        }
+
+        super.withdraw(sum);
+    }
+
 }
