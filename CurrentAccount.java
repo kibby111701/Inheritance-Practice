3683a1638c73685ba0900a2eb1de2bb0303c4eb7
Christopher Michael Kraft <kraftcm@s.dcsdk12.org>
2020-01-29 08:34:08 -0700

    finish accounts

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,1 +1,9 @@
-public class CurrentAccount extends Account{}
+public class CurrentAccount extends Account{
+    private double overdraftLimit;
+    
+    public CurrentAccount(int a, double o){
+        super(a);
+        this.overdraftLimit = o;
+    }
+
+}
--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,3 +1,12 @@
 public class SavingsAccount extends Account{
-    
+    private double interest;
+    public SavingsAccount(int a, double i){
+        super(a);
+        this.interest = i;
+    }
+
+
+    public void addInterest(){
+        super.deposit(getBalance() * interest);
+    }
 }
